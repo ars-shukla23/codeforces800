2201f16bbc41e75f780de558bf37be5b92e114d8
Aayush Shukla <aayush.shukla23@gmail.com>
2024-03-06 23:40:09 +0530

    Solved Word Capitalization on 6 Mar 24

---
+++ word-capital.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+
+
+int main(){
+    string s;
+    cin>>s;
+    if((int)s[0]>=97){
+        s[0]=s[0]-32;
+    }
+    cout<<s<<endl;
+
+
+    return 0;
+}
