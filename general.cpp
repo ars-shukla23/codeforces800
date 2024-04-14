0fceb17c5434a4d3532ea11b891867c472b6dda5
Aayush Shukla <aayush.shukla23@gmail.com>
2024-04-14 21:47:37 +0530

    Made corrections to General on 14 Apr 24

--- general.cpp
+++ general.cpp
@@ -10,11 +10,24 @@ int main(){
     for(int i=0;i<n;i++)cin>>vec[i];
     vector<int>vec2(vec);
     sort(vec2.begin(),vec2.end());
-    int maxn=vec2[n-1],minn=vec2[n-1];
+    int maxn=vec2[n-1],minn=vec2[0];
     int leftmax,rightmin;
-    for(int i=0;i<n;i++)if(vec[i]==maxn)leftmax=i;
-    for(int i=n-1;i>=0;i--)if(vec[i]==minn)rightmin=i;
-    cout<<leftmax+(n-rightmin)-2<<endl;
+    for(int i=0;i<n;i++){
+        if(vec[i]==maxn){
+        leftmax=i;
+        break;
+        }
+        }
+    for(int i=n-1;i>=0;i--){
+        if(vec[i]==minn){
+            rightmin=i;
+            break;
+            }
+        }
+
+    int result=(leftmax)+(n-1-rightmin);
+    if(leftmax>rightmin)result--;
+    cout<<result<<endl;
 
 
     return 0;
